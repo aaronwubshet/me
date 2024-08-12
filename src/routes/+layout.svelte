c2c8303570dd49fc792eecfce980a88fbe20bb94
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-08-11 20:03:28 -0700

    this is the one

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,27 +5,31 @@
 
   import "../style.css";
   let pages = [
-  { url: base +"/", title: 'Wubs', bases: base +"/", subPage: false },  
-  { url: base +'/resumeCV', title: 'Curriculum Vitae', bases: './resumeCV', subPage: false },
-  { url: base +'/art', title: "Art<sup>TM</sup>", bases: base +'/sayhello', subPage: false },
-  { url: base +'/sayhello', title: 'Contact', bases: base +'/sayhello', subPage: false },
-  { url: base +'/resumeCV/datavis', title: 'Who Owns Boston', base: base +'/resumeCV', subPage: true },
-  { url: base +'/resumeCV/DroneTracker', title: 'A Drone\'s Eye View', bases: base +'/resumeCV', subPage: true },
-  { url: base +'/resumeCV/SpeakerTracker', title: 'Mounted Speaker Tracking System', bases: base +'/resumeCV', subPage: true },
-  { url: base +'/resumeCV/6athesis', title: 'An Investigation of USRP FPGA as a platform for Quantum Sensing and Control', bases: base +'/resumeCV', subPage: true },
-  { url: base +'/resumeCV/astra', title: 'Astra Space Inc', bases: base +'/resumeCV', subPage: true },
-  { url: base +'/resumeCV/bain', title: 'Bain & Company', bases: base +'/resumeCV', subPage: true }
+  { url: base +"/", title: 'Wubs', subPage: false },  
+  { url: base +'/resumeCV', title: 'Curriculum Vitae', subPage: false },
+  { url: base +'/art', title: "Art<sup>TM</sup>", subPage: false },
+  { url: base +'/sayhello', title: 'Contact', subPage: false },
+  { url: base +'/resumeCV/datavis', title: 'Who Owns Boston', subPage: true },
+  { url: base +'/resumeCV/DroneTracker', title: 'A Drone\'s Eye View', subPage: true },
+  { url: base +'/resumeCV/SpeakerTracker', title: 'Mounted Speaker Tracking System', subPage: true },
+  { url: base +'/resumeCV/6athesis', title: 'An Investigation of USRP FPGA as a platform for Quantum Sensing and Control', subPage: true },
+  { url: base +'/resumeCV/astra', title: 'Astra Space Inc', subPage: true },
+  { url: base +'/resumeCV/bain', title: 'Bain & Company', subPage: true }
 
 ];
   import { page } from '$app/stores';
 
+  let s = "";
+  $: {
+    s= base+$page.route.id;
+  }
 
 </script>
 
 <nav>
   {#each pages as p}
     {#if !p.subPage}
-      <a href={ p.url } class:current={base + $page.route.id === p.url || ($page.route.id.includes(p.url)&&p.title!="Wubs")}> <Title title={p.title} /> </a>
+      <a href={ p.url } class:current={base + $page.route.id === p.url || (s.includes(p.url) && p.title!="Wubs")}> <Title title={p.title} /> </a>
     {/if}
   {/each}
 </nav>
