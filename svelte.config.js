98a2da400ada2a522dbc29440cec5126c00b9332
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-08-17 20:52:28 -0700

    maybe fix

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			base: process.env.NODE_ENV === 'production' ? '/me' : ''
+
 		},
 	},
 };
