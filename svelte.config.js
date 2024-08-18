1283623e9dfed1fa5a499dae4920d239422ca3cd
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-08-17 20:59:52 -0700

    hacky fix?

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
 			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-			base: process.env.NODE_ENV === 'production' ? 'me' : ''
+			base: process.env.NODE_ENV === 'production' ? '/me' : ''
 
 		},
 	},
