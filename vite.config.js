5a07c5cd56da3d2723cb55fc81bae48560484728
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-08-17 20:29:52 -0700

    fix?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: 'https://aaronwubshet.com/',
+	base: 'aaronwubshet.com',
 	plugins: [sveltekit()]
 });
