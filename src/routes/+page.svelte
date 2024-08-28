71a119c81e3712e1f6a92180ddb2fa6407087957
Aaron Wubshet <13680876+aaronwubshet@users.noreply.github.com>
2024-08-27 22:10:05 -0700

    more updates to front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,50 +14,46 @@
 <h2>The basics and more </h2>
 <p> Welcome to my drawing board / dumping ground / portfolio of past projects and experiences! If you've found yourself here then you probably know who I am, but if you don't, my name is Aaron. I will be your host for this journey through my (somewhat) professional life. But first a few quick facts...</p>
 <p>
-	<a href="https://youtu.be/UvlZFO0bQ6Y?t=353"><b>hOw wOuLd yOu dEsCrIbE YoUrSeLf</b></a> - I used to loathe this question, but that was probably because I didn't really like my own answer to it. Now I don't mind the question but still struggle to boil it down to a pithy sentence or two so here is a very non-MECE list of a dozen traits some of which I am actively working on improving.
+	<a href="https://youtu.be/UvlZFO0bQ6Y?t=353"><b>hOw wOuLd yOu dEsCrIbE YoUrSeLf</b></a> - I used to loathe this question, but that was probably because I didn't really like my own answer to it. Now I don't mind the question but still struggle to boil it down to a pithy sentence or two so here is a very non-MECE list of a few traits and a few key areas in which I am actively putting in effort to improve upon. 
 </p>
 <div style="display: flex;">
 	<div style="flex: 1;">
 		<ul>
+			<i>Key Traits</i>
 			<li>Independent</li>
 			<li>Organized</li>
 			<li>Empathetic</li>
 			<li>Adaptable</li>
-		</ul>
-	</div>
-	<div style="flex: 1;">
-		<ul>
 			<li>Cautious</li>
 			<li>Thoughtful</li>
 			<li>Curious</li>
 			<li>Relaxed</li>
 		</ul>
 	</div>
-</div>
-<p>
-	Moreover there are a few key areas in which I am actively putting in effort to improve upon. Things like:
-</p>
-	<ul>
-		<li>
-			<span class="tooltip">
-				<i>Leading a more healthy and active lifestyle - just ran my first 5K!</i>
-				<img src="images/5k.jpeg" alt="Aaron Wubshet" class="tooltip-image" />
-			</span>
-		</li>	
-		<li><i>Focus less on being less materialistic - added benefit of saving more money</i></li>
-		<li><i>Not always optimizing to find the path of least resistance - do the hard thing and struggle and maybe fail but definitely grow</i></li>
-		<li><i>Being more patient, jump to fewer conclusions, and keep an open mind - makes learning new things much easier and keeps the soul calm</i></li>
+	<div style="flex: 6;">
+		<ul>
+			<i>Know any good self-help books?</i>
+			<li>
+				<span class="tooltip">
+					<b>Leading a more healthy and active lifestyle</b> - <i>just ran my first 5K!</i>
+					<img src="images/5k.jpeg" alt="Aaron Wubshet" class="tooltip-image" />
+				</span>
+			</li>	
+			<li><b>Focus on being less materialistic</b> - <i>added benefit of saving more money</i></li>
+			<li><b>Don't always take the path of least resistance</b> - <i>do the hard thing. struggle. maybe fail. definitely grow</i></li>
+			<li><b>Being more patient, jump to fewer conclusions, keep an open mind</b> - <i>makes learning new things much easier</i></li>
+			<img src="images/selfhelp.jpeg" alt="Aaron Wubshet" style="width: 100%; height: 60%;" />
 
-	</ul>
+		</ul>
+	
+	</div>
+</div>
 
 	<p>
 		<b>"I don't got friends, I got<a href="https://www.youtube.com/watch?v=IQuc7wfO16Q">family</a>"</b> - My parents immigrated to the United States from Ethiopia before I was born in NOVA / DMV area. We soon moved the suburbs outside Atlanta where I grew up and my younger sister was born. While the majority of my extended family is still in Ethiopia, I have family in both places and the community vibrant and active, so I am grateful for the culture and values that have been instilled in me. 
 </p>
 <p>
-	<b>Lifelong learner</b> - Growing up I never really had the space or encouragement to explore hands on engineering driven passions. My parents mostly focused on ensuring I understood and learned the appropriate theory and conceptual ideas in the classroom. However, I was fortunate enough to go to a STEM focused high school called GSMST that did have a more robust engineering cirriculum. From there I went off to Boston to study at MIT<a href={resumeURL}> (and I kept coming back)</a>for my undergraduate degrees, a couple masters in electrical engineering, and an MBA. 
-	</p>
-	
-	<p>I continue to enjoy learning new things both related to engineering and in other domains. Here are a handful of things I am currently learning or want to learn more about!</p>
+	<b>Lifelong learner</b> - Growing up I never really had the space or encouragement to explore hands on engineering driven passions. My parents mostly focused on ensuring I understood and learned the appropriate theory and conceptual ideas in the classroom. However, I was fortunate enough to go to a STEM focused high school called GSMST that did have a more robust engineering curriculum. From there I went off to Boston to study at MIT<a href={resumeURL}> (and I kept coming back)</a>for my undergraduate degrees, a couple masters in electrical engineering, and an MBA. I continue to enjoy learning new things both related to engineering and in other domains. Here are a handful of things I am currently learning or want to learn more about!</p>
 		<div style="display: flex;">
 			<div style="flex: 1;">
 				<ul>
@@ -78,32 +74,32 @@
 		</div>
 
 <p>
-	<b>Hobbies / interests</b> - There are a few core activities that have always been part of
-	what I
-	like to use my free time like writing poetry and playing / watching basketball. However,
-	I've expanded my horizon of activities in recent years to a wide array of things from
-	watching anime to cooking and baking to learning how to play guitar or tennis or chess. I've
-	even found some time to get back into playing (in addition to streaming!) video games 
-	as well as learning more about photography.
+	<b>Hobbies & interests</b> - There are a few core activities that have always been part of how I use my free time like writing poetry and playing / watching basketball. However, I've grown and explored new activities over the years.</p>
+	<div style="display: flex;">
+		<div style="flex: 1;">
+			<ul>
+				<li>Anime</li>
+				<li>Photography</li>
+				<li>Watches</li>
+				<li>Sports (basketball, soccer, tennis)</li>
+				<li>Video games <a href= "https://www.youtube.com/@wubberino">(short lived streaming career)</a></li>
+				<li>Poetry (reading and writing)</li>
+				<li>Traveling</li>
+			</ul>
+		</div>
+		<div style="flex: 1;">
+			<ul>
+				<li>Listening to Audio books</li>
+				<li>Reading</li>
+				<li>Comedy</li>
+				<li>Board games</li>
+				<li>Red velvet</li>
+				<li>Persian and mediterranean food</li>
+			</ul>
+		</div>
+	</div>
 
-	
-</p>
-
-<p>
-	<b>Hobbies / interests</b> - There are a few core activities that have always been part of
-	what I
-	like to use my free time like writing poetry and playing / watching basketball. However,
-	I've expanded my horizon of activities in recent years to a wide array of things from
-	watching anime to cooking and baking to learning how to play guitar or tennis or chess. I've
-	even found some time to get back into playing (in addition to streaming!) video games 
-	as well as learning more about photography.
-</p>
-
-<p> As we jump into more detail about all things Aaron, I want to highlight the primary use case
-	of this site as simply exploring a few past projects and the resulting key skillsets I
-	gained as a result. My resume can also give you a quick flavor of my experiences and
-	skillset and is linked below. And if you want to chat, fill out the contact me form and 
-	I'll get back to you!
+<p> If you're still reading at this point then you're attention span has probably not been adversely impacted by short form video content too significantly so congratulations. Click around the website and you can get a better look at my work experience, projects (technical, business, and ~art~), skills (or lack thereof), and even send me an email via the contact me page. Or you can just email me at hello@aaronwubshet.com. But I worked hard trying to figure out how to make the the contact me page (so many AWS SES tutorials ໒( ಥ Ĺ̯ ಥ )७ ) so I'd appreciate if you use that. 
 </p>
 
 <img src="images/aaron.jpeg" alt="Aaron Wubshet" style = "width: 50%; height: 50%"/>
@@ -121,8 +117,8 @@
 
     .tooltip .tooltip-image {
         visibility: hidden;
-        width: 30%;
-        height: 600%;
+        width: 60%;
+        height: 1600%;
         background-color: white;
         text-align: center;
         border-radius: 6px;
